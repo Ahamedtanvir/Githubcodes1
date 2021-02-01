0286eb378304b221464b5c6ebdb7f9a8129f5cac
Ahamedtanvir <77918722+Ahamedtanvir@users.noreply.github.com>
2021-02-01 19:18:44 +0600

    Initial commit

---
+++ WT_Sp21_HTML_mid.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Info</title>
+</head>
+<body>
+
+<h2>Tanvir Ahamed </h2>
+<img src="mypic.jpg" width="200">
+<a href="https://www.facebook.com/sajid.ahmed.39">Link</a>
+<h3><ol>Education</h3>
+	<li>HSC</li>
+<li>SSC</li>
+<li>JSC</li>
+	
+
+	<h3><ul>Family</h3>
+	<li>Mother</li>
+<li>Father</li>
+	</ul>
+
+<h3><ul>Achivment</h3>
+	<li>Cricket</li>
+<li>Batminton</li>
+<li>Swimming</li>
+	</ul>
+	<li>
+	<h4>Tanvir Ahmed</h4>
+	<a href="https://github.com/tanvir-ahmed-aiub/WT_Sp21_L"></a>
+</li>
+</ol>
+</body>
+</html>
