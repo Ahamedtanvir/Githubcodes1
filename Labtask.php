c684a95f2d58d926720d1c50425e5d30f9a2323b
Ahamedtanvir <77918722+Ahamedtanvir@users.noreply.github.com>
2021-02-07 15:49:27 +0600

    Add files via upload

---
+++ Labtask.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php echo "A quick brown <b>fox</b> jumps over the lazy <b>dog</b>"?>
+
+
+</body>
+</html>
---
+++ lab1.3.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $name = "rasel";
+    if($name == "raju" || $name == "mina" || $name == "mithu")
+     {
+        echo "<b>hello</b>";
+     }
+
+
+    else echo "you are not recognized";
+
+     ?>
+
+  </body>
+</html>
